ac997a6b34852604298d39e391a4b51bf33e9717
iele <melephas@outlook.com>
2014-10-01 07:43:09 +0800

    ...

--- FanfouWP2/FanfouWP2.Shared/Utils/StatusesReform.cs
+++ FanfouWP2/FanfouWP2.Shared/Utils/StatusesReform.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Linq;
 using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
 
 namespace FanfouWP2.Utils
 {
@@ -11,51 +12,57 @@ namespace FanfouWP2.Utils
     {
         public static void reform(ObservableCollection<Status> statuses, List<Status> list)
         {
-            if (list.Count == 0)
-                return;
-
-            if (statuses.Count == 0)
+            lock (new Object())
             {
-                foreach (var item in list)
-                    statuses.Add(item);
-                return;
-            }
+                if (list.Count == 0)
+                    return;
 
-            if (list.Last().rawid > statuses.Last().rawid)
-            {
-                list.Reverse();
-                foreach (var i in list)
+                if (statuses.Count == 0)
                 {
-                    statuses.Insert(0, i);
-                }
-                return;
-            }
-
-            if (list.First().rawid < statuses.First().rawid)
-            {
-                foreach (var i in list)
-                {
-                    statuses.Add(i);
-                }
-                return;
-            }
-
-            for (var i = 0; i < list.Count; i++)
-            {
-                var j = 0;
-                for (j = 0; j < statuses.Count; j++)
-                {
-                    if (list[i].rawid < statuses[j].rawid)
-                        continue;
-                    if (list[i].rawid > statuses[j].rawid)
-                        break;
-                    if (list[i].rawid == statuses[j].rawid)
-                        goto equal;
+                    foreach (var item in list)
+                        statuses.Add(item);
+                    return;
                 }
 
-                statuses.Insert(j, list[i]);
-            equal:
-                continue;
+                if (list.Last().rawid > statuses.Last().rawid)
+                {
+                    list.Reverse();
+                    foreach (var i in list)
+                    {
+                        if ((from s in statuses where s.id == i.id select s).Count() == 0)
+                            statuses.Insert(0, i);
+                    }
+                    return;
+                }
+
+                if (list.First().rawid < statuses.First().rawid)
+                {
+                    foreach (var i in list)
+                    {
+                        if ((from s in statuses where s.id == i.id select s).Count() == 0)
+                            statuses.Add(i);
+                    }
+                    return;
+                }
+
+                for (var i = 0; i < list.Count; i++)
+                {
+                    var j = 0;
+                    for (j = 0; j < statuses.Count; j++)
+                    {
+                        if (list[i].rawid < statuses[j].rawid)
+                            continue;
+                        if (list[i].rawid > statuses[j].rawid)
+                            break;
+                        if (list[i].rawid == statuses[j].rawid)
+                            goto equal;
+                    }
+
+                    if ((from s in statuses where s.id == list[i].id select s).Count() == 0)
+                        statuses.Insert(j, list[i]);
+                equal:
+                    continue;
+                }
             }
         }
     }
