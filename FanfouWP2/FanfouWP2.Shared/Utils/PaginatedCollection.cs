58783dfde21c1522c2d390e19f5ad5840dccb846
iele <melephas@outlook.com>
2014-11-20 23:09:32 +0800

    ...

---
+++ FanfouWP2/FanfouWP2.Shared/Utils/PaginatedCollection.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI.Xaml.Data;
+
+namespace FanfouWP2.Utils
+{
+    public class PaginatedCollection<T> : ObservableCollection<T>, ISupportIncrementalLoading
+    {
+        public Func<uint, Task<IEnumerable<T>>> load;
+
+        private bool is_loading = false;
+        public bool HasMoreItems { get; protected set; }
+
+        public PaginatedCollection()
+        {
+            HasMoreItems = true;
+        }
+
+        public IAsyncOperation<LoadMoreItemsResult> LoadMoreItemsAsync(uint count)
+        {
+            return AsyncInfo.Run(async c =>
+            {
+                if (!is_loading)
+                {
+                    is_loading = true;
+                    try
+                    {
+                        var data = await load(count);
+
+                        foreach (var item in data)
+                        {
+                            Add(item);
+                        }
+
+                        //HasMoreItems = data.Any();
+
+                        return new LoadMoreItemsResult() { Count = (uint)data.Count() };
+                    }
+                    finally
+                    {
+                        is_loading = false;
+                    }
+                }
+                else
+                {
+                    return new LoadMoreItemsResult() { Count = 0 };
+                }
+            });
+        }
+    }
+}
