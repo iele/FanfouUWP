802b00f52906538ec89c2ba02870d398b43d8b14
iele <melephas@outlook.com>
2015-01-05 02:49:29 +0800

    ...

---
+++ FanfouWP2/FanfouWP2.Shared/Utils/ItemDataConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FanfouWP2.FanfouAPI.Items;
+using System.Runtime.Serialization.Json;
+using System.IO;
+
+namespace FanfouWP2.Utils
+{
+    public static class DataConverter<T>
+    {
+        public static string Convert(T item)
+        {
+            var ds = new DataContractJsonSerializer(typeof(T));
+            using (var ms = new MemoryStream())
+            {
+                ds.WriteObject(ms, item);
+                return System.Text.Encoding.UTF8.GetString(ms.ToArray(), 0, (int)ms.Length);
+            }
+        }
+
+        public static T Convert(string str)
+        {
+            var ds = new DataContractJsonSerializer(typeof(T));
+            using (var ms = new MemoryStream(Encoding.UTF8.GetBytes(str)))
+            {
+                return (T)ds.ReadObject(ms);
+            }
+        }
+    }
+}
